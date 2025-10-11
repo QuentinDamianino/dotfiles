aee1543ff74ac861d067dae32204c3b3ac1c1260
Damian Brdej <brdej98@gmail.com>
2025-10-11 14:14:09 +0200

    nvim: disable spell checks inside terminal

--- nvim/lua/custom/config/config.lua
+++ nvim/lua/custom/config/config.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'floaterm',
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
