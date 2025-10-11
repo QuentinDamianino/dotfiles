6d67df4941fe89cc352141cbf4b69d9bf690d4bc
Damian Brdej <brdej98@gmail.com>
2025-10-11 18:36:25 +0200

    install treesitter and add some languages

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,5 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",  -- Add this line
-    build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    branch = 'master',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'c', 'lua', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'phpdoc', 'javascript', 'typescript', 'css', 'html', 'blade', 'twig', 'yaml', 'json' },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+      })
+    end,
 }
