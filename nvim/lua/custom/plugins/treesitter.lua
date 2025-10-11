39eddd2d096412260c0a4a8ddbf19e3847a05da7
Damian Brdej <brdej98@gmail.com>
2025-10-11 20:40:42 +0200

    nvim: improve treesitter

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,35 +1,48 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    branch = 'master',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'c', 'lua', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'phpdoc', 'javascript', 'typescript', 'css', 'html', 'blade', 'twig', 'yaml', 'json' },
-        auto_install = true,
-        highlight = {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  branch = 'master',
+  dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { 'c', 'lua', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'php', 'phpdoc', 'javascript', 'typescript', 'css', 'html', 'blade', 'twig', 'yaml', 'json' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      textobjects = {
+        select = {
           enable = true,
+          lookahead = true,
+          keymaps = {
+            ['if'] = '@function.inner',
+            ['af'] = '@function.outer',
+            ['ia'] = '@parameter.inner',
+            ['aa'] = '@parameter.outer',
+          },
         },
-      })
+      },
+    })
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'php',
-        callback = function()
-          vim.bo.commentstring = '// %s'
-        end,
-      })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'php',
+      callback = function()
+        vim.bo.commentstring = '// %s'
+      end,
+    })
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'blade',
-        callback = function()
-          vim.bo.commentstring = '{{-- %s --}}'
-        end,
-      })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'blade',
+      callback = function()
+        vim.bo.commentstring = '{{-- %s --}}'
+      end,
+    })
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'twig',
-        callback = function()
-          vim.bo.commentstring = '{# %s #}'
-        end,
-      })
-    end,
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'twig',
+      callback = function()
+        vim.bo.commentstring = '{# %s #}'
+      end,
+    })
+  end,
 }
